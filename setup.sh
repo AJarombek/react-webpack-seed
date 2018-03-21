0593d0c24861623b4355ce8ca11ae1f632afe70a
Andrew Jarombek <ajarombek95@gmail.com>
2018-03-20 21:02:43 -0400

    1 - React Webpack Seed Hello World!

--- setup.sh
+++ setup.sh
@@ -20,3 +20,20 @@ yarn add babel-core --dev
 # Add both react and react-dom
 # react is the library used for creating views.  react-dom is used to render these views on the browser
 yarn add react react-dom
+
+# Install the webpack loader for Babel along with the presets.  Each preset specifies a transformation to perform on
+# the code
+yarn add babel-loader babel-preset-env babel-preset-react --dev
+
+# Required for handling HTML in Webpack
+yarn add html-webpack-plugin --dev
+yarn add html-loader --dev
+
+# Webpack 4 requires the webpack-cli package to run webpack from the command line
+yarn add webpack-cli --dev
+
+# You can run scripts defined in package.json.  Dev bundles with Webpack for the development environment
+yarn run dev
+
+# Build bundles for production
+yarn run build
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * The webpack configuration
+ * @author Andrew Jarombek
+ * @since 3/20/2018
+ */
+
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist/assets"),
+        filename: "bundle.js",
+        sourceMapFilename: "bundle.map"
+    },
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
